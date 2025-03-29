722c980eef3ee5c08737570ef38b9e60d82d6272
Noor-abid <nawazfatima292@gmail.com>
2025-03-29 21:34:28 +0500

    initialcommit

---
+++ simpleclasssyntax.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+class A{
+    private:
+    string name;
+    public:
+    void getin(){
+        cout<<"enter the name "<<endl;
+        cin>>name;
+    }
+    void display(){
+        cout<<"Name: "<<name;
+    }
+};
+int main(){
+    A obj;
+    obj.getin();
+    obj.display();
+}
